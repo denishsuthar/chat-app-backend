af39efa692e0cfa83d6573f9ba7efd3feaa36a18
Denish Suthar <denishsuthar04@gmail.com>
2023-02-17 15:09:08 +0530

    v1

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const socketIO = require("socket.io");
 
 const app=express();
-const port= process.env.PORT ;
+const port= 4000 || process.env.PORT ;
 
 
 const users=[{}];
 
 app.use(cors());
 app.get("/",(req,res)=>{
-    res.send("HELL ITS WORKING");
+    res.send("HELLO ITS WORKING");
 })
 
 const server=http.createServer(app);
@@ -25,7 +25,7 @@ io.on("connection",(socket)=>{
           users[socket.id]=user;
           console.log(`${user} has joined `);
           socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
-          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
+          socket.emit('welcome',{user:"Denish",message:`Welcome to the chat,${users[socket.id]} `})
     })
 
     socket.on('message',({message,id})=>{
